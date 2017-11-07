d92e3d5d00e4961a41ad986c08905e3b9e10ce91
haifei.wang <haifei.wang@baozun.com>
2017-11-07 15:52:37 +0800

    提交测试

--- src/test/T.java
+++ src/test/T.java
@@ -1,15 +1,9 @@
 package test;
 //123
 //abc
-<<<<<<< HEAD
-//abc
 //test1
-
-=======
-//abc
-
 //test4
->>>>>>> branch 'master' of https://github.com/haifei123/hello-world.git
+
 public class T {
 
 	private String var;
